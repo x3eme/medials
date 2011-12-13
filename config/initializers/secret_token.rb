d3cd54d5e7443ca33a24d39335d62bf952b16d84
shahin <shahin@gmail.com>
2011-12-13 16:20:12 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Medials::Application.config.secret_token = 'ff487b8fe7fbdd2abb41a7a07c147c947bf33702b43820102e27740eacab3b9d36c80899e7b2f268a374c6b6c930d91f7db2bfb2d7f945801cb942a3d077cb31'
